cdaf32b66aee86ffde5b16e35383dfc9f7fddf64
20191615JuneWon <71223432+20191615JuneWon@users.noreply.github.com>
2020-09-27 16:16:02 +0900

    20191615_no.1

---
+++ 20191615-Lab03/src/BasicIo.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class BasicIo {
+
+	public static void main(String[] args) {
+		Scanner stdin = new Scanner(System.in);
+		System.out.print("Enter your name: ");
+		String name = stdin.nextLine();
+		System.out.print("Enter your age: ");
+		int years = stdin.nextInt();
+		System.out.println("your name is " + name);
+		System.out.println("day: " + years*365);
+		System.out.print("how tall are you: ");
+		int height = stdin.nextInt();
+		System.out.print("what month,day,and year is now?(mm dd yyyy): ");
+		int month = stdin.nextInt();
+		int day = stdin.nextInt();
+		int year = stdin.nextInt();
+		System.out.printf("%d년%d월 %d일 현재 %s(%d)의 키는 %dcm 입니다",year,month,day,name,years,height );
+		
+
+	}
+
+}
---
+++ 20191615-Lab03/src/ConcatTester.java
@@ -0,0 +1,18 @@
+
+public class ConcatTester {
+
+	public static void main(String[] args) {
+		String animal1 = "quick brown fox";
+		String animal2 = "lazy dog";
+		String article = "the";
+		String action = "jumps over";
+		
+		
+		String answer = article.concat(animal1).concat(action).concat(animal2);
+		int num1 = answer.length();
+		System.out.println(answer);
+		System.out.print(num1);
+
+	}
+
+}
